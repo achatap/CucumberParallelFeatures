44925cbe7c0989211a1afe7d202ac1aee934c94e
Anurag Chatap <achatap@gmail.com>
2021-06-01 23:30:14 +0530

    headless

--- src/test/java/com/tau/pages/BasePage.java
+++ src/test/java/com/tau/pages/BasePage.java
@@ -4,6 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BasePage {
 
@@ -14,11 +22,41 @@ public String browser= "chrome";
 
     public WebDriver init(){
         if(browser.contains("chrome")){
+
+//            WebDriverManager.chromedriver().setup();
+//            DesiredCapabilities dc = DesiredCapabilities.chrome(); //or dc = DesiredCapabilities.chrome();
+//			ChromeOptions option = new ChromeOptions();
+//			option.addArguments("--no-sandbox");
+//			option.addArguments("--disable-dev-shm-usage");
+//			option.addArguments("--disable-gpu");
+////			option.addArguments("--headless");
+//			option.addArguments("--disable-notifications");
+////			option.addArguments("use-fake-ui-for-media-stream");
+//			dc.setCapability(ChromeOptions.CAPABILITY, option);
+
+//			try {
+//				driver= new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
+//			} catch (MalformedURLException e) {
+//				e.printStackTrace();
+//			}
             WebDriverManager.chromedriver().setup();
-            ChromeOptions options= new ChromeOptions();
-            options.addArguments("--headless");
-//            options.setBinary("/usr/bin/google-chrome");
-            driver= new ChromeDriver(options);
+            DesiredCapabilities dc = new DesiredCapabilities();
+            dc.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);//or dc = DesiredCapabilities.chrome();
+            ChromeOptions option = new ChromeOptions();
+			option.addArguments("--no-sandbox");
+			option.addArguments("--disable-dev-shm-usage");
+			option.addArguments("--disable-gpu");
+//			option.addArguments("--headless");
+			option.addArguments("--disable-notifications");
+//			option.addArguments("use-fake-ui-for-media-stream");
+			dc.setCapability(ChromeOptions.CAPABILITY, option);
+
+//            driver= new FirefoxDriver();
+            			try {
+				driver= new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), dc);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
 
 
         }
--- src/test/resources/google2.feature
+++
@@ -1,8 +0,0 @@
-Feature: This is simple google page search
-
-  Scenario: google page search
-    Given open url "https://facebook.com"
-    And open url "https://youtube.com"
-    And open url "https://gmail.com"
-    And open url "https://programesecure.com"
-#    When search for "facebook.com"
